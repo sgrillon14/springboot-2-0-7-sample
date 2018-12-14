060416eaea687a74321fc76e08daf4b614fbfe0a
Stéphane GRILLON <sgrillon14@users.noreply.github.com>
2018-12-14 14:44:36 +0000

    add code for valid MongoDatabase part

--- src/main/java/com/github/cloudyrock/mongock/springboot207sample/changesets/ClientChangeLog.java
+++ src/main/java/com/github/cloudyrock/mongock/springboot207sample/changesets/ClientChangeLog.java
@@ -1,21 +1,32 @@
 package com.github.cloudyrock.mongock.springboot207sample.changesets;
 
+import java.time.LocalDateTime;
+
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.springboot207sample.ClientDomain;
-
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.time.LocalDateTime;
+import com.mongodb.client.MongoDatabase;
 
 @ChangeLog
 public class ClientChangeLog {
 
     private static final String COLLECTION_NAME = "clients";
 
+    @ChangeSet(order = "001", id = "cs-1", author = "mongock")
+    public void changeSet1(final MongoDatabase db) {
+        db.getCollection(COLLECTION_NAME).insertOne(createMongoDocument(new ClientDomain("Gerorge1 " + LocalDateTime.now().toString(), "Orwell")));
+    }
+    
+    private Document createMongoDocument(ClientDomain clientDomain) {
+        return new Document()
+                .append("name", clientDomain.getName())
+                .append("surname", clientDomain.getSurname());
+    }
 
-
-    @ChangeSet(id = "cs-2", order = "002", author = "mongock")
+    @ChangeSet(order = "002", id = "cs-2", author = "mongock")
     public void changeSet2(MongoTemplate template) {
         template.insert(getClient(), COLLECTION_NAME);
     }
